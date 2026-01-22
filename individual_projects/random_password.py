cf9e4ac0993ed7f0d92586199aab3317c3da6fa0
guy <warrenjgib@gmail.com>
2026-01-22 16:08:24 +0000

    random password finished

--- individual_projects/random_password.py
+++ individual_projects/random_password.py
@@ -1,6 +1,6 @@
 #WG_CP2 random password generator
 #list of characters
-
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]
 #import random
 import random as r
 #import time as t
@@ -26,4 +26,21 @@ def main_menu():
         main_menu()
 
 #define random password
+def rand_pass():
     #inside this
+    length = input("How many characters do you want to be in your password? ")
+    if length.isdigit():
+        length = int(length)
+        password = ""
+        for i in range(length):
+            password += r.choice(characters)
+        print(f"Your random password is: {password}")
+        t.sleep(20)
+        print("\033c", end="")
+        main_menu()
+    else:
+        print("\033c", end="")
+        print("Please input a number.")
+        t.sleep(20)
+        rand_pass()
+main_menu()
