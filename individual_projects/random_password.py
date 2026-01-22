148f6a7e83def2e824380b767cf4a2208fe36161
guy <warrenjgib@gmail.com>
2026-01-22 16:23:38 +0000

    fixed random password

--- individual_projects/random_password.py
+++ individual_projects/random_password.py
@@ -1,6 +1,10 @@
 #WG_CP2 random password generator
 #list of characters
-characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"]
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "@","#", "$","%", "^","&","*", "(","-", "_","=", "+"]
+characters = lower + upper + numbers + symbols
 #import random
 import random as r
 #import time as t
@@ -32,8 +36,24 @@ def rand_pass():
     if length.isdigit():
         length = int(length)
         password = ""
+        if_lower = input("Do you want lowercase letters in your password? (y/n) ")
+        if_upper = input("Do you want uppercase letters in your password? (y/n) ")
+        if_numbers = input("Do you want numbers in your password? (y/n) ")
+        if_symbols = input("Do you want symbols in your password? (y/n) ")
+        possible_characters = []
+        if if_lower.lower() == "y":
+            possible_characters += lower
+        if if_upper.lower() == "y":
+            possible_characters += upper
+        if if_numbers.lower() == "y":
+            possible_characters += numbers
+        if if_symbols.lower() == "y":
+            possible_characters += symbols
+        if not possible_characters:
+            print("You must select at least one character type.")
+            return rand_pass()
         for i in range(length):
-            password += r.choice(characters)
+            password += r.choice(possible_characters)
         print(f"Your random password is: {password}")
         t.sleep(20)
         print("\033c", end="")
