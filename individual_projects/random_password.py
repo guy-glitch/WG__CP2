69c36410d4db0c858ac88d80ba9be5a4e333a73a
guy <warrenjgib@gmail.com>
2026-01-23 16:59:26 +0000

    fixed major errors

--- individual_projects/random_password.py
+++ individual_projects/random_password.py
@@ -4,14 +4,13 @@ lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "
 upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 symbols = ["!", "@","#", "$","%", "^","&","*", "(","-", "_","=", "+"]
-characters = lower + upper + numbers + symbols
 #import random
 import random as r
 #import time as t
 import time as t
 #define main menu function
 def main_menu():
-    print("Random password 1 \nExit 2")
+    print("\nRandom password 1 \nExit 2")
 
     type = input("Press the number that is alligned with the library function you want to use then press enter? ")
     if type.isdigit():
@@ -26,41 +25,60 @@ def main_menu():
     else:
         print("\033c", end="")
         print("Please input a number.")
-        t.sleep(20)
+        t.sleep(5)
         main_menu()
 
 #define random password
 def rand_pass():
     #inside this
     length = input("How many characters do you want to be in your password? ")
+    print("\033c", end="")
+
     if length.isdigit():
         length = int(length)
         password = ""
-        if_lower = input("Do you want lowercase letters in your password? (y/n) ")
-        if_upper = input("Do you want uppercase letters in your password? (y/n) ")
-        if_numbers = input("Do you want numbers in your password? (y/n) ")
-        if_symbols = input("Do you want symbols in your password? (y/n) ")
-        possible_characters = []
-        if if_lower.lower() == "y":
-            possible_characters += lower
-        if if_upper.lower() == "y":
-            possible_characters += upper
-        if if_numbers.lower() == "y":
-            possible_characters += numbers
-        if if_symbols.lower() == "y":
-            possible_characters += symbols
-        if not possible_characters:
-            print("You must select at least one character type.")
-            return rand_pass()
-        for i in range(length):
-            password += r.choice(possible_characters)
-        print(f"Your random password is: {password}")
-        t.sleep(20)
+        possible_characters = characters()
+
+        for i in range(4):
+            password_gen(length, possible_characters)
+        wait = input("Press enter to return to the main menu.")
         print("\033c", end="")
         main_menu()
+    
     else:
         print("\033c", end="")
         print("Please input a number.")
-        t.sleep(20)
+        t.sleep(5)
         rand_pass()
+
+#define characters function
+def characters():
+    if_lower = input("Do you want lowercase letters in your password? (y/n) ")
+    if_upper = input("Do you want uppercase letters in your password? (y/n) ")
+    if_numbers = input("Do you want numbers in your password? (y/n) ")
+    if_symbols = input("Do you want symbols in your password? (y/n) ")
+    possible_characters = []
+    print("\033c", end="")
+
+    if if_lower.lower() == "y":
+        possible_characters += lower
+    if if_upper.lower() == "y":
+        possible_characters += upper
+    if if_numbers.lower() == "y":
+        possible_characters += numbers
+    if if_symbols.lower() == "y":
+        possible_characters += symbols
+    if not possible_characters:
+        print("You must select at least one character type.")
+        characters()
+    return possible_characters
+#define password generator function
+def password_gen(length, possible_characters):
+    password = ""
+    
+    for i in range(length):
+        password += r.choice(possible_characters)
+    print(f"Here is your password option: \n{password}", end="\n\n")
+print("\033c", end="")
+#call main menu
 main_menu()
