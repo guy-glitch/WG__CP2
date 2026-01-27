35ad53081318b3752a6bc8ca444cb8f6c913e5e3
guy-glitch <warren.gibson@ucas-edu.net>
2026-01-27 09:51:38 -0700

    started morse code generator

---
+++ individual_projects/morse_alpha_translate.py
@@ -0,0 +1,52 @@
+#WG_CP2 morse code translator
+#import time as t and random as r
+import time as t
+#a tuple of all the alphabet letters
+alpha_code = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+#a tuple of all the morse code letters
+morse_code = (".-", "-...", "-.-.", "-..", ".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".---.","--.-",".-.","...","-","..-","...-",".---","-..-","-.--","--..")
+
+#define a main menu function
+def menu():
+#use match to let them check if they choose to enter the morse code translator
+    while True:
+        action = int(input("Do you want to translate something nto morse code? press 1 \nDo you want to translate something into english from morse code? press 2\nOr exit. press 3"))
+        match action:
+            case 1:
+                morse_alpha()
+            case 2:
+                alpha_morse()
+            case 3:
+                exit()
+
+#define morse code to alphabet
+def morse_alpha():
+    character = original()
+    if character in morse_code:
+        print(f"{alpha_code[list.index(character)]} is {character} in english")
+        t.sleep("\033c", end="")
+        return character
+    else:
+        print("That is not morse code please try again, or it is not something that codes for a letter.")
+        t.sleep(3)
+        print("\033c", end="")
+        morse_alpha()
+
+#define the original function to get the thing they want to translate
+def original():
+    translate = input("What do you want to translate, if you are doing morse code please enter one character at a time. ").strip().upper()
+    t.sleep(3)
+    print("\033c", end="")
+    return translate
+
+#define the alphabet to code
+def alpha_morse():
+    word = original()
+    characters = list(word)
+    seperator = ""
+    if characters[1] in alpha_code:
+        morse_values = []
+        for i in len(characters):
+            morse_values.append(alpha_code[i-1])
+        morse_values = seperator.join(map(str, morse_values))
+        print
