55e5c1dec797f51bd43a044f5c7a810d1e32c74f
guy-glitch <warren.gibson@ucas-edu.net>
2026-02-02 09:51:53 -0700

    reading csv

--- Notes/reading.py
+++ Notes/reading.py
@@ -1,5 +1,5 @@
 #WG_CP2 Reading a file 
-
+import csv
 try:
     with open("Notes/read_file.txt", "r") as file:
         for line in file:
@@ -10,3 +10,18 @@ except:
 
 else:
     print("Code ends")
+
+try:
+    with open("Notes/sample.csv", mode = "r") as sample:
+        content = csv.reader(sample)
+        headers = next(content)
+        rows = []
+        for line in content:
+           rows.append({headers[0]: line[0], headers[1]: line[1]})
+
+except:
+    print("That file can't be found.")
+else:
+    for line in rows:
+        for key in line:
+            print(f"{key}: {line[key]}")
