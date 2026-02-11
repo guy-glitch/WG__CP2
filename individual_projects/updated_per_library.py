5a86d899904888452dc9926348776e149567a2db
guy-glitch <warrenjgib@gmail.com>
2026-02-11 09:47:42 -0700

    updated how the library function works

--- individual_projects/updated_per_library.py
+++ individual_projects/updated_per_library.py
@@ -3,12 +3,20 @@ import csv
 #import time as t
 import time as t
 #library with books
+try:
+    with open("individual_projects/library.csv", mode = "r") as library:
+        content = csv.reader(library)
+        headers = next(content)
+        library = []
+        for line in content:
+           library.append({headers[0]: line[0], headers[1]: line[1], headers[2]: line[2], headers[3]: line[3]})
 
+except:
+    print("The library is having trouble loading please close and try again.")
+    exit()
 
 #define main menu function
-def main_menu():
-    print("View 1 \nAdd 2  \nRemove 3 \nSearch 4 \nExit 5")
-    type = input("Press the number that is alligned with the library function you want to use then press enter? ")
+def main_menu(library):
     #use match to let them check if they choose to enter the morse code translator
     while True:
         print("View 1 \nAdd 2  \nRemove 3 \nSearch 4 \nExit 5")
@@ -16,10 +24,16 @@ def main_menu():
         match type:
             case "1":
                 print("\033c", end="")
-                display()
+                display(library)
             case "2":
                 print("\033c", end="")
-                add()
+                add(library)
+            case "3":
+                print("\033c", end="")
+                remove(library)
+            case "4":
+                print("\033c", end="")
+                search(library)
             case "5":
                 print("\033c", end="")
                 exit()
@@ -30,46 +44,40 @@ def main_menu():
                 continue
 
 #define a function that displays everything in the vile
-def display():
+def display(library):
     exit = 1
     while exit!="y":
-        library = view()
-        for i in library:
-            print(i)
+        view(library)
         exit = input("Do you want to exit. y/n")
     return exit
 
 #define the view function
-def view():
-
-    try:
-        with open("individual_projects/library.csv", mode = "r") as library:
-            content = csv.reader(library)
-            rows = []
-            for line in content:
-                rows.append(line[0])
-            return rows
-
-    except:
-        print("\033[31mThere Was an ERROR finding the library.")
-   
+def view(library):
+    for i in range(len(library)):
+        if isinstance(library[i], dict):
+            for j in library[i]:
+                print(f"{j}:{library[i][j]}", end=" ")
+        print()
+    exit = input("Do you want to exit only enter something if you want to exit. ")
     print("\033c", end="")
-    main_menu()
+    main_menu(library)
 
 #define the add function that adds a book to the library
-def add():
+def add(library):
     #get the title and the author than add it to the library
-    title = input("What is the title of the book.").strip().title()
-    by = input("Who wrote the book?").strip().title()
-    book = f"{title} by {by}"
-    library.add(book)
+    title = input("What is the title of the book. ").strip().title()
+    by = input("Who wrote the book? ").strip().title()
+    year = input("When was this book published? ")
+    genre = input("What is the genre of the book?")
+    book = {"title":title, "author":by}
+    library.append(book)
 
     print(f"{book} added to the library")
     print("\033c", end="")
-    main_menu()
+    main_menu(library)
 #define the function that removes a book from the library
 
-def remove():
+def remove(library):
     title = input("What is the title or author of the book you want to remove? ").strip().title()
     in_library = any(title in item for item in library)
 
@@ -80,16 +88,16 @@ def remove():
                 print(f"{book} has been removed from the library. ")
                 t.sleep(3)
                 print("\033c", end="")
-                main_menu()
+                main_menu(library)
 
     else:
         print(f"{title} is not in the library, make sure it is spelled correctly next time. ")
         print("\033c", end="")
-        main_menu()
+        main_menu(library)
 
 #define a function that allows for you to search the library
 
-def search():
+def search(library):
     title = input("What do you want to search for in the library? ").title().strip()
     in_library = any(title in item for item in library)
 
@@ -101,7 +109,7 @@ def search():
                 print(f"{book} is in the library.")
                 t.sleep(5)
                 print("\033c", end="")
-                main_menu()
+                main_menu(library)
     else:
         print(title, " is not in the library")
 
@@ -115,4 +123,4 @@ def exitp():
     print("\033c", end="")
     quit()
 
-main_menu()
+main_menu(library)
