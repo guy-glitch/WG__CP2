79cbd29b5e98cceede6f7bddaaf165b7daaa9f28
guy-glitch <warrenjgib@gmail.com>
2026-03-18 09:44:20 -0600

    wanted to save fast just pseudocode

---
+++ individual_projects/classes/main.py
@@ -0,0 +1,9 @@
+#WG_CP2 Classes project 
+
+#main menu function 
+    #Use case
+    #case 1
+    #case 2
+    #case 3
+
+#call main menu
---
+++ individual_projects/classes/pet_control.py
@@ -0,0 +1,15 @@
+#import time and csv, along with clear screen from login
+#class Pet
+    #name
+    #type dog, cat, fish, lizard
+    #Age in months
+    #set their health, energy, happiness, and hunger to a random whole number between 1,100
+    #based off of the user profiles last pet num
+
+#A dictionary of all of the pet types as keys, and within each value have a dictionary of the possible food items for that species and the value it increases stats by
+
+#A function that save the pet under its number in the pet status csv
+
+#A function that takes in what food they selected and changes the stats of their pet by that before overwriting their current stats
+
+#A function that increases their stats based off of sleep and overwrites their previous stats
