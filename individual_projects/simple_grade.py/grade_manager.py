00fa77c134c33f72b03e83a7323eeeec9eecdbd4
guy-glitch <warrenjgib@gmail.com>
2026-03-27 09:05:13 -0600

    adf

--- individual_projects/simple_grade.py/grade_manager.py
+++ individual_projects/simple_grade.py/grade_manager.py
@@ -1,5 +1,4 @@
 #The grade for each student grade managment
-import json
 #A class for the gradebook
     #__init__ self students set students to an empty list
     #Add a student method take in the name, student ID
@@ -8,6 +7,8 @@ import json
 
 #A class for each student
     #initilize self student ID & student name
+    #A function that sets a grade for that student
+    #A function that 
 #Veiw grade for student function that shows them a list of the students and lets them view the full student information
 #A function that displays all of their students and the grades of those students.
 #
