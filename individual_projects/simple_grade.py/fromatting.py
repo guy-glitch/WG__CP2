d741e5a9de317a451dff61733196e731f9cf1f42
guy-glitch <warrenjgib@gmail.com>
2026-03-27 09:52:47 -0600

    asdf

--- individual_projects/simple_grade.py/fromatting.py
+++ individual_projects/simple_grade.py/fromatting.py
@@ -1,7 +1,12 @@
 #import time, and json
 import time as t, json
 #A clear screen instant function
+def wipe():
+    print("\033c", end="")
 #A clear screen read time funtion
+def read_wipe():
+    input("Press enter to continue....")
+    wipe()
 #A function that turns an object into a dictionary then adds it to a json
 def dict_transform(object):
     def object_to_dict(obj):
@@ -12,3 +17,18 @@ def dict_transform(object):
     return obj_dict
     
 #A function that returns a letter grade based off of the actual grade of the student
+def return_letter(grade):
+    try:
+        int(grade)
+    except:
+        return "grade is not a number"
+    if grade >= 94:
+        return "A"
+    elif grade >= 80:
+        return "B"
+    elif grade >=70:
+        return "C"
+    elif grade >=60:
+        return "D"
+    else:
+        return "F"
--- individual_projects/simple_grade.py/grade_manager.py
+++ individual_projects/simple_grade.py/grade_manager.py
@@ -1,15 +1,16 @@
 #The grade for each student grade managment
 #A class for the gradebook
+class Gradebook:
     #__init__ self students set students to an empty list
+    def __init__(self, students = []):
+        self.students = students
     #Add a student method take in the name, student ID
-        #create new student using the student class
-    #
+        #create new student using the student class Method
+    #search take in a student ID and display the student Method
+    #Display all of their students and the grade they have along with the avergage grade. Method
+    #A method that allows for you to type in a student ID and change their grade
 
 #A class for each student
     #initilize self student ID & student name
     #A function that sets a grade for that student
     #A function that 
-#Veiw grade for student function that shows them a list of the students and lets them view the full student information
-#A function that displays all of their students and the grades of those students.
-#
-    
